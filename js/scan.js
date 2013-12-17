43108f884c217e255c6aef7a110fcc0e35d211c4
Akhil <akhil.madanan@nlindia.com>
2013-12-17 09:26:30 +0530

    sd
    sdf

---
+++ js/scan.js
@@ -0,0 +1,17 @@
+function scan()
+{
+    
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+    
+    
+}
